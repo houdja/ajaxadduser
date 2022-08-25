431d781a29c90e73c7d7a7acd9e20828d5b1826f
Houdja <houdja-ahamadi976@hotmail.com>
2022-08-25 19:10:10 +0400

    affichage du message et vidage des input si ajout réussi

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let form = document.querySelector('.form');
+let inputs = form.querySelectorAll('input');
+let msg = document.querySelector('.msg');
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    msg.innerHTML = 'Chargement...';
 
     let xhr = new XMLHttpRequest();
     let data = new FormData(this);
@@ -10,6 +13,16 @@ form.addEventListener('submit', function(e){
         if(xhr.readyState === 4){
             if(xhr.status == 200){
                 console.log(xhr.response);
+                let res = xhr.response;
+                
+                if(res.success != false){
+                    inputs.forEach(input => {
+                        input.value = '';
+                    })
+                }
+
+                msg.innerHTML = res.msg;
+
             }else{
                 alert('Une erreur est survenue...');
             }
